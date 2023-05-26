471941d487f88f7323318cbb33e9b1d510729c4f
Javad Bahrami <javadev14bh@gmail.com>
2023-05-26 20:30:00 +0330

    Add infinite scroll

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,45 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+
+//Infinite scroll
+import InfiniteScroll from "react-infinite-scroll-component";
 
 //Skeleton
-import CoinSkeleton from './CoinSkeleton';
+import CoinSkeleton from "./CoinSkeleton";
 
 //API
-import { getCoins } from '../services/api';
+import { getCoins } from "../services/api";
 
 //Gif
-import loader from '../assets/loader.gif'
+import loader from "../assets/loader.gif";
 
 //Components
-import Coin from './Coin';
+import Coin from "./Coin";
 
 //Styles
-import styles from './Landing.module.css';
+import styles from "./Landing.module.css";
+import axios from "axios";
 
 const Landing = () => {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("market_cap_desc");
+  const [hasMore , setHasMore] = useState(true);
+  const [page , setPage] = useState(2);
 
-    const [coins , setCoins] = useState([]);
-    const [search , setSearch] = useState('');
-    const [filter,setFilter] = useState("market_cap_desc");
+  useEffect(() => {
+    const fetchAPI = () => {
+      getCoins(filter)
+        .then((data) => setCoins(data))
+        .catch((error) =>
+          alert(`${error.message} : Check your connection or try again later`)
+        );
+    };
+    fetchAPI();
+  }, [filter]);
 
-    useEffect(() => {
-        const fetchAPI =  () => {
-            getCoins(filter)
-            .then(data => setCoins(data))
-            .catch( error => alert(`${error.message} : Check your connection or try again later`))
-        }
-        fetchAPI()
-    },[filter])
+  console.log(coins)
 
-    const searchHandler = event => {
-        setSearch(event.target.value);
-    }
-    const searchedCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
+  const fetchCoins = async () => {
+    const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=${filter}&per_page=50&page=${page}&sparkline=false&locale=en`);
+    return res.data
+  };
 
-    const filterHandler = event => {
-        setFilter(event.target.value)
-    }
+  const fetchData = async () => {
+   const coinsForServer = await fetchCoins();
+   setCoins([...coins, ...coinsForServer]);
+   if(coinsForServer === 0 || coinsForServer < 20){
+    setHasMore(false);
+   }
+   setPage(page + 1);
+  }
 
-    return (
+  const searchHandler = (event) => {
+    setSearch(event.target.value);
+  };
+  const searchedCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const filterHandler = (event) => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <InfiniteScroll
+      style={{textAlign : "center"}}
+      dataLength={coins.length}
+      next={fetchData}
+      hasMore={hasMore}
+      loader={<img style={{width : "250px"}} src={loader} alt="loader"/>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
+      {
         <div className={styles.container}>
             <p style={{textAlign : 'center' , marginTop : '15px'}}>Made by <a href="https://github.com/javadevbh/" target="blank">Javad Bahrami</a>&#127775;</p>
             <input className={styles.input} type='text' placeholder='Search coins' value={search} onChange={searchHandler}/>
@@ -74,7 +112,9 @@ const Landing = () => {
                 }
             </div>
         </div>
-    );
+      }
+    </InfiniteScroll>
+  );
 };
 
 export default Landing;
--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,9 @@ import axios from "axios";
 
 
 const getCoins = async (filter) => {
-    const BASE_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=${filter}&per_page=250&page=1&sparkline=false&locale=en`;
+    const BASE_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=${filter}&per_page=50&page=1&sparkline=false&locale=en`;
     const response = await axios.get(BASE_URL);
     return response.data
 }
 
 export {getCoins}
-
-//market_cap_asc, market_cap_desc, volume_asc, volume_desc, id_asc, id_desc
